2be0e60c92fbc884a8b164f46d7c62e1d20da3b8
eg00 <egor@povshok.ru>
2020-12-05 22:54:11 +0500

    User password update

--- app/Http/Controllers/User/SettingsController.php
+++ app/Http/Controllers/User/SettingsController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
+use App\Rules\MatchOldPassword;
 use Grimzy\LaravelMysqlSpatial\Types\Point;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class SettingsController extends Controller
 {
@@ -25,13 +28,20 @@ class SettingsController extends Controller
         $location = new Point($request->location['latitude'], $request->location['longitude']);
 
 
-        $user->update(array_merge($request->toArray(),compact('location')));
+        $request->user()->update(array_merge($request->toArray(),compact('location')));
 
         return new UserResource($user);
     }
 
     public function updatePassword(Request $request)
     {
+        $this->validate($request, [
+            'current_password' => ['required', new MatchOldPassword],
+            'password' => ['required', 'confirmed', 'min:6', 'different:current_password'],
+        ]);
 
+        $request->user()->update(['password' => Hash::make($request->password)]);
+
+        return response()->json(['message'=> 'Password updated'], Response::HTTP_OK);
     }
 }
